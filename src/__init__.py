13794067cb86827f219aa76de8c7ad20f6e862e4
Jim137 <jiangjim@gapp.nthu.edu.tw>
2023-04-21 19:59:08 +0800

    get result and cost estimation from azure

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,1 @@
-from .request_handler import azure_provider
+from .request_handler import azure_provider, cost_estimation, get_result
--- src/request_handler.py
+++ src/request_handler.py
@@ -1,4 +1,6 @@
 from azure.quantum.qiskit import AzureQuantumProvider
+from qiskit import QuantumCircuit
+from qiskit.tools.monitor import job_monitor
 import json
 
 
@@ -18,6 +20,22 @@ def azure_provider():
     )
     return provider
 
+def cost_estimation(circuit: QuantumCircuit, backend_name: str="quantinuum.qpu.h1-2", shots: int = 100):
+    "Returns the cost estimation of the circuit on the specified backend."
+
+    provider = azure_provider()
+    backend = provider.get_backend(backend_name)
+    cost = backend.estimate_cost(circuit, shots=100)
+    return cost
+
+def get_result(circuit: QuantumCircuit, backend_name: str="quantinuum.sim.h1-2sc", shots: int = 100):
+    "Returns the result of the circuit on the specified backend."
+
+    provider = azure_provider()
+    backend = provider.get_backend(backend_name)
+    job = backend.run(circuit, shots=shots)
+    job_monitor(job)
+    return job.result()
 
 if __name__ == "__main__":
     "Test whether the provider is working and print the available targets."
